3f2a87b762018d5fcd94c9b21adaf89485942b4f
Dunja Lalic <dunja.lalic@gmail.com>
2019-08-13 22:12:48 +0200

    Removed needless type annotation

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod models;
 mod serializers;
 
 pub fn run(filename: String) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(filename)?;
+    let contents = fs::read_to_string(filename)?;
     let resume: Resume = serde_yaml::from_str(&contents).unwrap();
     let mut file = File::create("resume.tex")?;
     match file.write_all(&resume.render().unwrap().as_bytes()) {
