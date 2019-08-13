e62db5c925a4a01efa53702adc0abf02a4eca412
Sven Neumann <sven@svenfoo.org>
2019-08-13 22:02:18 +0200

    Implement FromStr trait for Color
    Tests are missing, but at least it does compile now ...

--- src/models/color.rs
+++ src/models/color.rs
@@ -1,5 +1,8 @@
 use core::fmt;
 use serde::Deserialize;
+use std::error;
+use std::str::FromStr;
+use std::num::ParseIntError;
 
 #[derive(Debug, Deserialize)]
 pub struct Color {
@@ -11,5 +14,52 @@ pub struct Color {
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}, {}, {}", self.red, self.green, self.blue)
+    }
+}
+
+impl FromStr for Color {
+    type Err = ParseColorError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut values: [u8; 3] = [ 0; 3 ];
+        for (i, c) in s.split_terminator(",").enumerate() {
+            match i {
+                0|1|2 => values[i] = u8::from_str(c)?,
+                _ => return Err(ParseColorError::WrongNumberOfComponents)
+            }
+        }
+        let color: Color = Color{red: values[0], green: values[1], blue: values[2]};
+        Ok(color)
+    }
+}
+
+#[derive(Debug)]
+pub enum ParseColorError {
+    WrongNumberOfComponents,
+    Parse(ParseIntError)
+}
+
+impl fmt::Display for ParseColorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ParseColorError::WrongNumberOfComponents =>
+                write!(f, "wrong number of color components, 3 expected"),
+            ParseColorError::Parse(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for ParseColorError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            ParseColorError::WrongNumberOfComponents => None,
+            ParseColorError::Parse(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<ParseIntError> for ParseColorError {
+    fn from(err: ParseIntError) -> ParseColorError {
+        ParseColorError::Parse(err)
     }
 }
--- src/serializers/color_serializer.rs
+++ src/serializers/color_serializer.rs
@@ -1,28 +1,13 @@
 use crate::models::color::Color;
 use serde::{self, Deserialize, Deserializer};
-use serde_yaml::from_str;
+use std::str::FromStr;
 
 pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Color, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let raw_string: String = String::deserialize(deserializer)?;
-    let components: Vec<&str> = raw_string.split_terminator(",").collect();
-
-    fn color_value(component : &str) -> Result<u8, &'static str> {
-       let value = from_str(component).unwrap();
-       match value {
-           0..=255 => Ok(value as u8),
-           _ => Err("color value out of range")
-       }
-    }
-
-    let color: Color = Color {
-        red: color_value(components[0]).unwrap(),
-        green: color_value(components[1]).unwrap(),
-        blue: color_value(components[2]).unwrap()
-    };
-    Ok(color)
+    let string = String::deserialize(deserializer)?;
+    Color::from_str(&string).map_err(serde::de::Error::custom)
 }
 
 #[cfg(test)]
