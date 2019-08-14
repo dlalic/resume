1d7a67e1fba3553cc7f94e6f30db1f134d99b548
Dunja Lalic <dunja.lalic@gmail.com>
2019-08-14 20:42:06 +0200

    Added layout config

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,6 +2,7 @@ pub mod color;
 pub mod education;
 pub mod employment;
 pub mod experience;
+pub mod layout_config;
 pub mod person;
 pub mod resume;
 pub mod skill;
--- src/models/resume.rs
+++ src/models/resume.rs
@@ -2,6 +2,7 @@ extern crate askama;
 use crate::models::education::Education;
 use crate::models::employment::Employment;
 use crate::models::experience::Experience;
+use crate::models::layout_config::LayoutConfig;
 use crate::models::person::Person;
 use crate::models::skill::Skill;
 use askama::Template;
@@ -10,6 +11,8 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize, Template)]
 #[template(path = "resume.tex", escape = "none")]
 pub struct Resume {
+    #[serde(default)]
+    pub(crate) config: LayoutConfig,
     pub(crate) person: Person,
     pub(crate) employments: Vec<Employment>,
     pub(crate) educations: Vec<Education>,
