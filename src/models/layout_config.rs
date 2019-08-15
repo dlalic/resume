d2f31c31e38994417059ee064cac2c33d62c69da
Dunja Lalic <dunja.lalic@gmail.com>
2019-08-15 19:35:52 +0200

    Added OTF fonts and updated layout

--- src/models/layout_config.rs
+++ src/models/layout_config.rs
@@ -3,13 +3,15 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct LayoutConfig {
     pub(crate) font: String,
+    pub(crate) font_location: String,
     pub(crate) font_size: String,
 }
 
 impl Default for LayoutConfig {
     fn default() -> Self {
         LayoutConfig {
-            font: "Avenir Next".to_string(),
+            font_location: "./examples/fonts/".to_string(),
+            font: "FiraSans".to_string(),
             font_size: "12pt".to_string(),
         }
     }
