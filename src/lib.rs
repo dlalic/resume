7e156a1037b828d2c8d2887fdc32d0f9a8a6a63b
Dunja Lalic <dunja.lalic@gmail.com>
2019-08-18 14:00:31 +0200

    Moved cl parsing back to main per code review feedback

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use crate::integrations::execute::Execute;
 use crate::integrations::sn::SnIntegration;
 use crate::integrations::tex::TexIntegration;
 use crate::models::resume::Resume;
-use clap::ArgMatches;
 use std::error::Error;
 use std::fs;
 
@@ -12,12 +11,17 @@ mod integrations;
 pub mod models;
 mod serializers;
 
-pub fn run(matches: &ArgMatches, config: &str) -> Result<String, Box<dyn Error>> {
+#[derive(Debug)]
+pub enum Integration {
+    Tex,
+    Sn,
+}
+
+pub fn run(int: Integration, config: &str) -> Result<String, Box<dyn Error>> {
     let contents = fs::read_to_string(config)?;
     let resume: Resume = serde_yaml::from_str(&contents)?;
-    match Option::from(matches.subcommand_name()) {
-        Some("tex") => TexIntegration::execute(&resume),
-        Some("sn") => SnIntegration::execute(&resume),
-        _ => Ok("Please specify a subcommand. Use --help for help.".to_string()),
+    match int {
+        Integration::Tex => TexIntegration::execute(&resume),
+        Integration::Sn => SnIntegration::execute(&resume),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@
 extern crate clap;
 
 use clap::App;
+use resume::Integration;
+
+fn from_subcommand(matches: &clap::ArgMatches) -> Option<Integration> {
+    match Option::from(matches.subcommand_name()) {
+        Some("tex") => Some(Integration::Tex),
+        Some("sn") => Some(Integration::Sn),
+        _ => None,
+    }
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -9,8 +18,12 @@ fn main() {
 
     let config = matches.value_of("config").unwrap_or("examples/resume.yaml");
     println!("Using config: {}", config);
-    match resume::run(&matches, config) {
-        Ok(output) => println!("{}", output),
-        Err(e) => println!("Error: {:?}", e)
+    if let Some(int) = from_subcommand(&matches) {
+        match resume::run(int, config) {
+            Ok(output) => println!("{}", output),
+            Err(e) => println!("Error: {:?}", e),
+        }
+    } else {
+        println!("Please specify a subcommand. Use --help for help.");
     }
 }
