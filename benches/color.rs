dde4d3a920d3c3a3d2a102fb72595bd058d04883
Sven Neumann <sven@svenfoo.org>
2019-09-18 18:59:43 +0200

    Formatting (#30)
    * Formatting, by means of cargo fmt
    
    * Add cargo-husky
    
    See https://github.com/rhysd/cargo-husky
    
    Configure it so that it installs a git pre-push hook that verifies
    that all tests pass and that the code is properly formatted.
    
    * Configure Travis CI to check formatting
    
    * Apply changes suggested by clippy
    
    * Run formatting check on the stable build as well

--- benches/color.rs
+++ benches/color.rs
@@ -1,16 +1,16 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
 use criterion::black_box;
+use criterion::Criterion;
 
 use resume::models::color::Color;
 use std::str::FromStr;
 
 fn color_from_string_benchmark(c: &mut Criterion) {
-    c.bench_function("Color::from_str", |b| b.iter(||
-        Color::from_str(black_box("123, 42, 23"))
-    ));
+    c.bench_function("Color::from_str", |b| {
+        b.iter(|| Color::from_str(black_box("123, 42, 23")))
+    });
 }
 
 criterion_group!(color, color_from_string_benchmark);
