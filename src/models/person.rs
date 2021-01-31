519343784915ae9b8c7257bd4aa679d2f2ac9af7
Dunja Lalic <dunja.lalic@gmail.com>
2021-01-31 20:57:06 +0100

    Add pronoun

--- src/models/person.rs
+++ src/models/person.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct Person {
     pub(crate) name: String,
+    pub(crate) pronoun: String,
     pub(crate) location: String,
     pub(crate) contact: String,
     pub(crate) image_file: Option<String>,
