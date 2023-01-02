6dfd13605e788995efcd363dabb03d594c613efb
Dunja Lalic <dunja.lalic@gmail.com>
2023-01-02 01:07:54 +0100

    Fix clippy warning

--- src/operations/write_to_file.rs
+++ src/operations/write_to_file.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 use std::path::Path;
 
 pub(crate) fn write_to_file(path: &Path, contents: &[u8]) -> Result<String> {
-    let mut file = File::create(&path)?;
+    let mut file = File::create(path)?;
     file.write_all(contents)?;
     Ok(format!("Wrote {}", path.display()))
 }
